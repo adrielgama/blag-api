2412b269940c4ebde899ecafc3f8f45c4118a9c3
Adriel Gama <adrielgama@gmail.com>
2023-10-06 18:59:27 -0300

    feat: add cors policy

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import cron from 'node-cron'
-import { router } from './routes'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { router } from './routes'
 import { clearExpiredTokens } from './cron/clearExpiredTokens'
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use(express.json())
 app.use(router)
