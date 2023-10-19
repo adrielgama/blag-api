a2e4ad731e64d355aeaf717f1d9dcb2b2a938cd0
Adriel Gama <adrielgama@gmail.com>
2023-10-19 18:08:41 -0300

    fix: adjustments to cors origins

--- src/server.ts
+++ src/server.ts
@@ -11,25 +11,13 @@ const port = process.env.PORT || 3000
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://blag.adrielgama.dev/',
-  'https://www.josianemendonca.adv.br/',
-  'https://josianemendonca.adv.br/',
-  'blag.adrielgama.com/',
+  'https://blag.adrielgama.dev',
+  'https://www.josianemendonca.adv.br',
 ]
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true)
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          'A política CORS para este site não permite acesso a partir da origem especificada.'
-        return callback(new Error(msg), false)
-      }
-
-      return callback(null, true)
-    },
+    origin: allowedOrigins,
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     credentials: true,
   }),
